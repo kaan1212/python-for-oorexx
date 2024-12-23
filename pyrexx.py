a3b2fed88ecaf2b2bc5b1954077b9050a7b3686c
Kaan <119818534+kaan1212@users.noreply.github.com>
2024-12-23 22:33:39 +0100

    Add from clause, object attribute read access, py~123 shorthand, py~kwd(keyword, value) shorthand, debug output, and error messages.

--- pyrexx.py
+++ pyrexx.py
@@ -3,7 +3,26 @@ import importlib
 import sys
 
 
+debug_enabled = False
 objects = {}
+
+
+# Public.
+def debug_on():
+    global debug_enabled
+    debug_enabled = True
+
+
+# Public.
+def debug_off():
+    global debug_enabled
+    debug_enabled = False
+
+
+# Private.
+def debug(*args):
+    if debug_enabled:
+        print(*args)
 
 
 # Public.
@@ -34,8 +53,16 @@ def invoke_str(identity):
 
 
 # Public.
-def invoke_import(module_name):
-    globals()[module_name] = importlib.import_module(module_name)
+def invoke_import(name):
+    globals()[name] = importlib.import_module(name)
+
+
+# Public.
+def invoke_from_import(name, fromlist):
+    module = __import__(name, globals(), locals(), fromlist, 0)
+
+    for name in fromlist:
+        globals()[name] = getattr(module, name)
 
 
 # Public.
@@ -52,13 +79,19 @@ def invoke_function(name, arguments):
 # Public.
 def invoke_method(identity, name, arguments):
     identity = int(identity)
-    name = name.lower()
     object = objects[identity]
-    function = getattr(object, name)
+    name = name.lower()
 
-    object = invoke_function_with_arguments(function, arguments)
-    identity = id(object)
-    objects[identity] = object
+    try:
+        value = getattr(object, name)
+    except AttributeError as err:
+        print('AttributeError:', err)
+
+    if callable(value):
+        value = invoke_function_with_arguments(value, arguments)
+
+    identity = id(value)
+    objects[identity] = value
 
     return str(identity)
 
@@ -66,37 +99,88 @@ def invoke_method(identity, name, arguments):
 # Private.
 def resolve_function(name):
     name = name.lower()
+    debug('Resolving', name)
 
-    if '.' in name:
-        module_name, function_name = name.rsplit('.', 1)
-        module = globals()[module_name]
+    function = resolve_in_globals(name)
 
-        for attribute in dir(module):
-            if function_name.lower() == attribute.lower():
-                function = getattr(module, attribute)
-                break
-    elif name in globals():
-        function = globals()[name]
+    if function != None:
+        return function
     elif hasattr(builtins, name):
-        function = getattr(builtins, name)
+        debug('    Built-in identifier')
+        return getattr(builtins, name)
     else:
-        sys.exit('Function not found:', name)
+        print('Function not found:', name)
+        sys.exit(1)
 
-    return function
+
+# Private.
+def resolve_in_globals(name):
+    # Shortcut for exact match.
+    if name in globals():
+        value = globals()[name]
+        debug('    globals():', name)
+        return value
+    elif '.' not in name:
+        return None
+
+    # Greedy search of possibly hierarchical module name in globals().
+    # Example:
+    #   datetime.datetime.now
+    #   module.class.classmethod
+    left = name
+    while True:
+        index = left.rfind('.')
+        if index == -1:
+            return None
+
+        left = left[:index]
+        if left in globals():
+            value = globals()[left]
+            debug('    globals():', value)
+            break
+
+    # Right part.
+    right = name[len(left)+1:]
+
+    for name in right.split('.'):
+        value = getattr_caseless(value, name)
+        debug('    Attribute:', value)
+
+    return value
+
+
+# Private.
+def getattr_caseless(object, name):
+    name = name.lower()
+    matches = [attr for attr in dir(object) if attr.lower() == name]
+
+    match len(matches):
+        case 1:
+            return getattr(object, matches[0])
+        case 0:
+            print(f'Error: {object} has no attribute {name}')
+            sys.exit(1)
+        case _:
+            print(f'Error: {object} has no unambiguous attribute {name}: {matches}')  # nopep8
+            sys.exit(1)
 
 
 # Private.
 def invoke_function_with_arguments(function, arguments):
-    if contains_kwargs(arguments):
-        args = arguments[:-1]
-        kwargs = arguments[-1]
+    try:
+        if contains_kwargs(arguments):
+            args = arguments[:-1]
+            kwargs = arguments[-1]
 
-        kwargs = kwargs.copy()
-        del kwargs['kwargs']
+            kwargs = kwargs.copy()
+            del kwargs['kwargs']
 
-        return function(*args, **kwargs)
-    else:
-        return function(*arguments)
+            return function(*args, **kwargs)
+        else:
+            return function(*arguments)
+    except TypeError as err:
+        print('TypeError:', err)
+        sys.exit(1)
 
 
 # Private.
